01b8d1ddee2fe88069bbcf26ee746c0b305402d0
joaovaz94 <j>
2021-08-02 16:25:02 -0300

    Finalizando o tutorial

--- src/main/kotlin/com/example/blog/HtmlController.kt
+++ src/main/kotlin/com/example/blog/HtmlController.kt
@@ -1,6 +1,6 @@
 package com.example.blog
 
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.server.ResponseStatusException
 
 @Controller
-class HtmlController(private val repository: ArticleRepository) {
+class HtmlController(private val repository: ArticleRepository,
+                                        private val properties: BlogProperties) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
-        model["title"] = "Blog"
+        model["title"] = properties.title
+        model["banner"] = properties.banner
         model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render()}
         return "blog"
     }
@@ -23,7 +25,7 @@ class HtmlController(private val repository: ArticleRepository) {
         val article = repository
             .findBySlug(slug)
             ?.render()
-            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+            ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
         model["title"] = article.title
         model["article"] = article
         return "article"
